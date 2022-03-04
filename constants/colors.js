8725d1589009b87fd47c4871532ea8d0d0ba263f
As-Amin <asadamin1313@gmail.com>
2022-03-04 20:36:24 +0000

    Changed colour theme

--- constants/colors.js
+++ constants/colors.js
@@ -1,8 +1,8 @@
 export const Colors = {
-  text: '#D8DEE9',
+  text: '#FFFFFF',
   darkerBackground: '#000000',
   lighterBackground: '#1e1e1e',
   lineBreak: '#1e1e1e',
-  theme: '#5c97bf',
+  theme: '#7c9de3',
   error: '#BF616A',
 };
